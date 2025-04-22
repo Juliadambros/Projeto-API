c59456dd806abe6e00487f509a1a2bf807652fc5
juliadambros <julia.dambros.ju@gmail.com>
2025-04-22 16:38:49 -0300

    22_04

--- src/shared/http/router/index.ts
+++ src/shared/http/router/index.ts
@@ -1,10 +1,8 @@
+import productsRouter from "@modules/products/routes/products.routes";
 import { Router } from "express";
 
 const routes = Router();
+routes.use('/products', productsRouter);
 
-routes.get('/', (request, response) => {
-    response.json({message: 'Hello Dev!'});
-    return;
-});
 
 export default routes;
