991a35ed719a9c42a6ad49ab1917d138b059d801
juliadambros <julia.dambros.ju@gmail.com>
2025-05-13 15:59:46 -0300

    13_05

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,9 +2,23 @@ import { Router } from "express";
 import UsersController from "../controllers/UsersController";
 import { celebrate, Joi, Segments } from "celebrate";
 import isAuthenticated from "../../../shared/http/middlewares/isAuthenticated";
+import multer from "multer";
+import uploadConfig from '@config/upload';
+import UserAvatarController from "../controllers/UserAvatarController";
 
 const usersRoutes = Router();
 const usersController =  new UsersController();
+
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
+usersRoutes.patch('/avatar', isAuthenticated, upload.single('avatar'), async(req, res, next)=>{
+    try{
+    await userAvatarController.update(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
 
 usersRoutes.get('/', isAuthenticated, async(req, res, next)=>{
     try{
