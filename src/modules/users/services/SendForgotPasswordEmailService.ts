e734d8b439563666cdf87e66ea14253341ddbff3
juliadambros <julia.dambros.ju@gmail.com>
2025-05-20 16:24:51 -0300

    20_05

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,27 +1,39 @@
+import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
+import path from 'path';
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
-import AppError from "@shared/errors/AppError";
 import EtherealMail from "@config/mail/EtherealMail";
 
 interface IRequest{
-    email: string;
+  email: string;
 }
 
 export default class SendForgotPasswordEmailService{
-    public async execute({email} : IRequest): Promise<void>{
-        const userRepository = getCustomRepository(UsersRepository);
-        const userTokensRepository = getCustomRepository(UserTokensRepository);
-        const user = await userRepository.findByEmail(email);
-        if(!user){
-            throw new AppError('User does not exist.');
-        }
-        const {token} = await userTokensRepository.generate(user.id);
-        console.log(token);
-        await EtherealMail.sendMail({
-            to: email,
-            body: `Solicitação de refinição de senha recebida ${token}`
-        })
+
+  public async execute({ email }: IRequest) : Promise<void>{
+    const usersRepository = getCustomRepository(UsersRepository);
+    const usersTokensRespository = getCustomRepository(UserTokensRepository);
+    const forgotPasswordTemplete = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
+    const user = await usersRepository.findByEmail(email);
+    if(!user){
+      throw new AppError('User does not exists.');
     }
 
+    const {token} = await usersTokensRespository.generate(user.id);
+
+    console.log(token);
+    await EtherealMail.sendMail({
+      to: {name: user.name, email: user.email}, 
+      subject: '[API VENDAS] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplete,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`, 
+        },
+      },
+  });
+  }
 }
